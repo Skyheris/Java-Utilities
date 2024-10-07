17cc0041afd2604669c8c98b09122e96b62ab049
Skybryne <Kiko.oliveira2020@outlook.pt>
2024-10-07 10:36:02 +0100

    Recursive Exercise

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
         int[] v = new int[10];
         for (int i = 0; i < v.length; i++){
             v[i] = i + 2;
-        }
+        } // 2,3,4,5,6,7,8,9,10,11
         System.out.println(arraySum(v));
-        System.out.println(arrayPower(10, 2));
+        System.out.println(power(10, 5));
+        System.out.println(powerRecursive(5, 3));
+        System.out.println(maxValue(v));
     }
 
     public static int arraySum(int[] n){
@@ -19,13 +21,39 @@ public class Main {
             return n[pos] + arraySum(n, ++pos);
     }
 
-    public static int arrayPower(int n, int pow){
+    public static int power(int n, int pow){
         if (pow == 0)
             return 1;
-        int s = 0;
+        int s = 1;
         for (int i = 0; i < pow; i++){
-            s = n*n;
+            s *= n;
         }
         return s;
+    }
+
+    public static double powerRecursive(int n, int pow){
+        return powerRecursiveFunc(n, pow);
+    }
+
+    protected static double powerRecursiveFunc(int n, int pow){
+        if (pow == 0)
+            return 1;
+        else if (n % 2 == 0)
+            return powerRecursiveFunc(n*n, pow/2);
+        else {
+            return n*powerRecursiveFunc(n*n, pow/2);
+        }
+    }
+    public static int maxValue(int[] n){
+        return maxValue(n[0], n, 0);
+    }
+    protected static int maxValue(int tempMax, int[] n, int pos){
+        if (n.length == 1)
+            return n[pos];
+        if (pos == n.length)
+            return tempMax;
+        if (tempMax < n[pos])
+            tempMax = n[pos];
+        return maxValue(tempMax, n, ++pos);
     }
 }
