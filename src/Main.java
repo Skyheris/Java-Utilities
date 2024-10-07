506ce2bd397a62fbcf7e0f85bdb5603f8bc139c2
Skybryne <Kiko.oliveira2020@outlook.pt>
2024-10-07 09:52:46 +0100

    Recursive Exercise

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
             v[i] = i + 2;
         }
         System.out.println(arraySum(v));
+        System.out.println(arrayPower(10, 2));
     }
 
     public static int arraySum(int[] n){
@@ -16,5 +17,15 @@ public class Main {
             return 0;
         else
             return n[pos] + arraySum(n, ++pos);
+    }
+
+    public static int arrayPower(int n, int pow){
+        if (pow == 0)
+            return 1;
+        int s = 0;
+        for (int i = 0; i < pow; i++){
+            s = n*n;
+        }
+        return s;
     }
 }
